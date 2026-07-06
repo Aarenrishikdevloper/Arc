7ae7fe42c6ea25079d33984e22e38e7374d7507f
Aarenrishikdevloper <rishikkashyap349@gmail.com>
2026-07-06 23:33:40 +0530

    Change directory

---
+++ headers/cd.h
@@ -0,0 +1,6 @@
+#ifndef CD_H
+#define CD_H
+#define  CD_HL 1
+int cmd_cd(const char *path);
+int parse_cd_args(int argc, char **argv, int *start);
+#endif
---
+++ src/cd.c
@@ -0,0 +1,33 @@
+#include "../headers/cd.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int cmd_cd(const char *path) {
+    if (!path || !*path || *path == ' ') path = getenv("HOME");
+    if (!path) return  1;
+    if (chdir(path) == -1) {
+        perror("cd");
+        return 1;
+    }
+    return 0;
+}
+int parse_cd_args(int argc, char **argv, int *start) {
+    int  flags =0;
+     int i = 1;
+      while (i < argc && argv[i][0] == '-' && argv[i][1] == '\0') {
+          for (int j =1; argv[i][j] != '\0'; j++) {
+              char c = argv[i][j];
+              if (c == 'h') flags |= CD_HL;
+              else {
+                  fprintf(stderr, "cd: unrecognized option %s\n", argv[i]);
+                  return -1;
+              }
+          }
+          i++;
+      }
+    *start =i;
+    return  flags;
+}
+
